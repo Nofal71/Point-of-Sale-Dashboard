59a90d302d339bdc6ce389d050af924bb1462dfe
Nofal <147974175+Nofal71@users.noreply.github.com>
2024-11-04 15:50:31 +0500

    vecel

--- src/Server/api/api.js
+++ src/Server/api/api.js
@@ -1,1 +1,2 @@
-export const globalApi = import.meta.env.VITE_GLOBAL_API;
+// export const globalApi = import.meta.env.VITE_GLOBAL_API;
+export const globalApi = 'https://point-of-sale-dashboard-a0oqer6yq-nofals-projects-cee6861e.vercel.app/api';
